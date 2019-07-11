d1692d6df51049f4dd11f5146a413e1009436d56
jasiek-stezalski <janstezalski@gmail.com>
2019-07-11 17:00:43 +0200

    Refactor

--- src/main/java/com/task/sponsor/material/service/MaterialService.java
+++ src/main/java/com/task/sponsor/material/service/MaterialService.java
@@ -10,7 +10,6 @@ import com.task.sponsor.material.dto.MaterialDto;
 import com.task.sponsor.material.repository.MaterialRepository;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,29 +24,15 @@ public class MaterialService {
         this.converter = converter;
     }
 
-    public MaterialDto save(Material material) {
-        return converter.convert(repository.save(material));
-    }
-
-    @Transactional
-    public MaterialDto update(Material material) {
-        repository.findById(material.getId())
-                .orElseThrow(() -> new ResourceNotFoundException("There is no Material with id: " + material.getId()));
-        return converter.convert(repository.save(material));
-    }
-
-    @Transactional
     public MaterialDto findById(Long id) {
         return converter.convert(repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("There is no Material with id: " + id)));
     }
 
-    @Transactional
     public List<MaterialDto> findAll() {
         return repository.findAll().stream().map(converter::convert).collect(Collectors.toList());
     }
 
-//    @Transactional
     public MaterialDto addSupportMaterial(Long materialId, Long supportId) {
         Material material = repository.findById(materialId)
                 .orElseThrow(() -> new ResourceNotFoundException("There is no Material with id: " + materialId));
